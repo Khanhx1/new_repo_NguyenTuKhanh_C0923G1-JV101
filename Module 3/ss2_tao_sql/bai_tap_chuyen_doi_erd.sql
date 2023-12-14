4645ec04ee94a5e67eb1aa40608639f7bb4ca7c1
Khanhx1 <nguyentukhanh2001@gmail.com>
2023-12-15 02:03:06 +0700

    i

--- Module 3/ss2_tao_sql/bai_tap_chuyen_doi_erd.sql
+++ Module 3/ss2_tao_sql/bai_tap_chuyen_doi_erd.sql
@@ -4,19 +4,19 @@ use database_2;
 
 create table phieu_xuat(
 so_phieu_xuat varchar(7) primary key,
-ngay_xuat date
+ngay_xuat date not null
 );
 
 create table vat_tu(
 ma_vat_tu varchar(7) primary key,
-ten_vat_tu varchar(50)
+ten_vat_tu varchar(50) not null
 );
 
 create table chi_tiet_phieu_xuat(
 ma_vat_tu varchar(7),
-so_phieu_xuat varchar(7),
-don_gia_xuat bigint, 
-so_luong_xuat int,
+so_phieu_xuat varchar(7) not null,
+don_gia_xuat bigint default 0, 
+so_luong_xuat int default 0,
 primary key(ma_vat_tu, so_phieu_xuat),
 foreign key(ma_vat_tu) references vat_tu(ma_vat_tu),
 foreign key(so_phieu_xuat) references phieu_xuat(so_phieu_xuat),
@@ -25,14 +25,14 @@ constraint dieu_kien_1 check(don_gia_xuat >= 0 and so_luong_xuat >= 0)
 
 create table phieu_nhap(
 so_phieu_nhap varchar(7) primary key,
-ngay_nhap date
+ngay_nhap date not null
 );
 
 create table chi_tiet_phieu_nhap(
 ma_vat_tu varchar(7),
 so_phieu_nhap varchar(7),
-don_gia_nhap bigint,
-so_luong_nhap int,
+don_gia_nhap bigint default 0,
+so_luong_nhap int default 0,
 primary key(ma_vat_tu, so_phieu_nhap),
 foreign key(ma_vat_tu) references vat_tu(ma_vat_tu),
 foreign key(so_phieu_nhap) references phieu_nhap(so_phieu_nhap),
@@ -41,8 +41,8 @@ constraint dieu_kien_2 check(don_gia_nhap >= 0 and so_luong_nhap >= 0)
 
 create table nha_cung_cap(
 ma_nha_cc varchar(7) primary key,
-ten_nha_cc varchar(45),
-dia_chi varchar(50)
+ten_nha_cc varchar(45) not null,
+dia_chi varchar(50) not null
 );
 
 create table don_dat_hang(
@@ -50,7 +50,7 @@ so_don_hang varchar(7),
 ma_nha_cc varchar(7),
 primary key(so_don_hang, ma_nha_cc),
 foreign key (ma_nha_cc) references nha_cung_cap(ma_nha_cc),
-ngay_dat_hang date
+ngay_dat_hang date not null
 );
 
 create table chi_tiet_don_dat_hang(
